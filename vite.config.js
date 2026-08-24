10cf0efc473252a2c5347eea46d84e5db49ae048
toussaint1122 <your.ishimwedidierk@gmail.com>
2026-08-24 18:10:36 +0200

    working

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  resolve: {
+    dedupe: ["react", "react-dom"],
+  },
 });
