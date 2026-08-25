0226075b6405beeb1f340cdd5c255937cbebf73a
toussaint1122 <your.ishimwedidierk@gmail.com>
2026-08-25 17:14:06 +0200

    we

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+   base: "Porfolio-react-app",
   plugins: [react(), tailwindcss()],
   resolve: {
     dedupe: ["react", "react-dom"],
